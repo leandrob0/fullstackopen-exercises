dbf21e68e0a3848d7f135d98ff100f886cb1a2fb
leandrob0 <leandro.bovino@gmail.com>
2021-12-31 18:47:00 -0300

    step 2.1 to 2.5 done

---
+++ part2/CourseInfo-continuation/src/components/Course.js
@@ -0,0 +1,38 @@
+const Title = ({ title }) => {
+    return (
+        <>
+            <h2>{title}</h2>
+        </>
+    )
+}
+
+const Parts = ({ parts }) => {
+    return (
+        <ul>
+            {
+                parts.map(val => <li key={val.id}>{val.name} {val.exercises}</li>)
+            }
+            <p>Total exercises: {parts.reduce((prev,cur) => prev + cur.exercises ,0)}</p>
+        </ul>
+    )
+}
+
+const Course = ({ courses }) => {
+
+    return (
+        <div>
+            {
+                courses.map(val => {
+                    return (
+                        <>
+                            <Title key={val.id} title={val.name} />
+                            <Parts parts={val.parts} />
+                        </>
+                    )
+                })
+            }
+        </div>
+    )
+}
+
+export default Course;
