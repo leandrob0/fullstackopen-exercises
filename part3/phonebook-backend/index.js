81385f01ce47cc6a5f613aae03833048d56698cd
leandrob0 <leandro.bovino@gmail.com>
2022-01-14 02:17:30 -0300

    phonebook-backend step8 done

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -25,7 +25,9 @@ let phonebook = [
   },
 ];
 
-app.use(morgan('tiny'));
+morgan.token('posted', (req, res) => JSON.stringify(req.body));
+
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :posted'));
 app.use(express.json());
 
 // Get complete phonebook
