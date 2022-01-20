ba4251d6eeb3dd14951ed8d35efc7865708e18f8
leandrob0 <leandro.bovino@gmail.com>
2022-01-19 21:25:17 -0300

    eslint added to the project

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,54 +1,54 @@
-require('dotenv').config();
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
-const Person = require("./models/person");
-const app = express();
+require('dotenv').config()
+const express = require('express')
+const morgan = require('morgan')
+const cors = require('cors')
+const Person = require('./models/person')
+const app = express()
 
-morgan.token('posted', (req, res) => JSON.stringify(req.body));
+morgan.token('posted', (req) => JSON.stringify(req.body))
 
-app.use(express.static('build'));
-app.use(cors());
-app.use(express.json());
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :posted'));
+app.use(express.static('build'))
+app.use(cors())
+app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms - :posted'))
 
 // Get complete phonebook
-app.get("/api/persons", (req, res) => {
+app.get('/api/persons', (req, res) => {
   Person.find({}).then(persons => {
-    res.json(persons);
+    res.json(persons)
   })
-});
+})
 
 // Get info of phonebook
-app.get("/info", (req, res) => {
+app.get('/info', (req, res) => {
   Person.find({}).then(persons => {
     res.send(`Phonebook has info of ${persons.length} people <br> ${new Date()}`)
   })
-});
+})
 
 // Delete specific person
-app.delete("/api/persons/:id", (req, res) => {
+app.delete('/api/persons/:id', (req, res, next) => {
   Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end();
+    .then(() => {
+      res.status(204).end()
     })
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 // Add a new person
-app.post("/api/persons", (req, res, next) => {
-  const body = req.body;
+app.post('/api/persons', (req, res, next) => {
+  const body = req.body
 
   const person = new Person({
     name: body.name,
     number: body.number,
-  });
+  })
 
   person.save()
     .then(savedPerson => savedPerson.toJSON())
     .then(savedAndFormatted => res.json(savedAndFormatted))
-    .catch(err => next(err));
-});
+    .catch(err => next(err))
+})
 
 app.put('/api/persons/:id', (request, response, next) => {
   const body = request.body
@@ -58,25 +58,25 @@ app.put('/api/persons/:id', (request, response, next) => {
     number: body.number,
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true , runValidators: true})
+  Person.findByIdAndUpdate(request.params.id, person, { new: true , runValidators: true })
     .then(updatedPerson => {
       response.json(updatedPerson)
     })
     .catch(error => next(error))
-});
+})
 
 // Get a specific person
-app.get("/api/persons/:id", (req, res, next) => {
+app.get('/api/persons/:id', (req, res, next) => {
   Person.findById(req.params.id)
     .then(person => {
       if(person) {
-        res.json(person);
+        res.json(person)
       } else {
-        res.send(404).end();
+        res.send(404).end()
       }
     })
     .catch(err => next(err))
-});
+})
 
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
@@ -97,7 +97,7 @@ const errorHandler = (error, request, response, next) => {
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/phonebook-backend/models/person.js
+++ part3/phonebook-backend/models/person.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
-console.log("connecting to", url);
+const url = process.env.MONGODB_URI
+console.log('connecting to', url)
 
 mongoose
   .connect(url)
-  .then((result) => {
-    console.log("connected to MongoDB");
+  .then(() => {
+    console.log('connected to MongoDB')
   })
   .catch((error) => {
-    console.log("error connecting to MongoDB:", error.message);
-  });
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
   name: {
@@ -25,18 +25,18 @@ const personSchema = new mongoose.Schema({
     minLength: 8,
     required: true,
   },
-});
+})
 
 // makes sure the said property of the schema is unique.
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
 // function to format the saved to mongo object to send it to the frontend.
-personSchema.set("toJSON", {
+personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString();
-    delete returnedObject._id;
-    delete returnedObject.__v;
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
   },
-});
+})
 
-module.exports = mongoose.model("Person", personSchema);;
+module.exports = mongoose.model('Person', personSchema)
