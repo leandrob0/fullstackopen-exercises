c5508153d630af46de67719570de02ad1e3eda7e
leandrob0 <leandro.bovino@gmail.com>
2022-02-01 22:39:22 -0300

    mongouri deleted

--- part4/blog-list/utils/config.js
+++
@@ -1,7 +0,0 @@
-const PORT = 3003;
-const mongo = 'mongodb+srv://fullstackopen:Xf0smhrtefa0abLR@cluster0.ffi0k.mongodb.net/bloglist?retryWrites=true&w=majority';
-
-module.exports = {
-    PORT,
-    mongo
-}
